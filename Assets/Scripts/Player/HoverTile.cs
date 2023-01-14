d8d9998c91e52479b2567519eb8a03520df96c34
JackShort <john.short.tj@gmail.com>
2023-01-14 16:46:31 -0500

    inventory hover placer

--- Assets/Scripts/Player/HoverTile.cs
+++ Assets/Scripts/Player/HoverTile.cs
@@ -5,34 +5,44 @@ public class HoverTile : MonoBehaviour {
     [SerializeField] private Tilemap tileMap;
     [SerializeField] private Grid grid;
     [SerializeField] private Map map;
+    [SerializeField] private Inventory inventory;
+    [SerializeField] private GameObject itemPlacer;
 
     private Camera _main;
-    private Vector3Int _previouslyHoveredCell;
+    private Vector3Int _previouslyHoveredTileCoordinates;
 
     private void Start() {
         _main = Camera.main;
     }
 
+    // TODO: make function on item placer that updates the sprite etc. this is hacky
     private void Update() {
-        var hoveredCell = MapUtilities.GetHoveredTileCoordinates(_main, grid);
-        if (hoveredCell == _previouslyHoveredCell) {
+        var hoveredTileCoordinates = MapUtilities.GetHoveredTileCoordinates(_main, grid);
+
+        if (hoveredTileCoordinates == _previouslyHoveredTileCoordinates) {
             return;
         }
 
-        if (tileMap.GetTile(_previouslyHoveredCell)) {
-            var resource =
-                MapUtilities.GetResourceAtTileCoordinates(_previouslyHoveredCell, map);
-
-            if (resource) {
-                tileMap.SetColor(_previouslyHoveredCell, resource.color);
+        if (!MapUtilities.CheckIfTilePositionIsInMapBounds(hoveredTileCoordinates, map)) {
+            if (itemPlacer.activeSelf) {
+                itemPlacer.SetActive(false);
             }
-        }
 
-        _previouslyHoveredCell = hoveredCell;
-        if (!tileMap.GetTile(hoveredCell)) {
             return;
         }
 
-        tileMap.SetColor(hoveredCell, Color.yellow);
+        if (!itemPlacer.activeSelf) {
+            itemPlacer.SetActive(true);
+        }
+
+        var worldCoordinates = MapUtilities.GetWorldPositionFromTileCoordinates(hoveredTileCoordinates, grid);
+        itemPlacer.transform.position = new Vector3(worldCoordinates.x + 0.5f, worldCoordinates.y + 0.5f, 0);
+
+        var activeItem = inventory.GetActiveItem();
+        if (activeItem != null) {
+            itemPlacer.GetComponent<SpriteRenderer>().sprite = activeItem.sprite;
+        }
+
+        _previouslyHoveredTileCoordinates = hoveredTileCoordinates;
     }
 }
--- Assets/Scripts/Scriptable Objects/Inventory.cs
+++ Assets/Scripts/Scriptable Objects/Inventory.cs
@@ -1,5 +1,16 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Custom/Inventory")]
 public class Inventory : RuntimeSet<Item> {
+    public int activeIndex;
+
+    [CanBeNull]
+    public Item GetActiveItem() {
+        if (items.Count == 0) {
+            return null;
+        }
+
+        return activeIndex >= items.Count || activeIndex < 0 ? items[0] : items[activeIndex];
+    }
 }
